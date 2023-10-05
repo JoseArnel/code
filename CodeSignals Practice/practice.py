f21036e099566ea59b28aae3a693402c3c706044
Jose Arnel <jonel.manipon@gmail.com>
2023-10-05 16:01:31 -0400

    code

---
+++ CodeSignals Practice/practice.py
@@ -0,0 +1,7 @@
+# in preperation for Captial One Assessment 
+
+def checkPalindrome(inputString):
+    for i in range(0,int(len(inputString))//2):
+        if inputString[i] != inputString[len(inputString)-i-1]:
+            return False
+    return True
