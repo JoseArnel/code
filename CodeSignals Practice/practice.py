ee760f7f635d232a0897aa3f2a42b663c663c38f
Jose Arnel <jonel.manipon@gmail.com>
2023-10-06 17:24:56 -0400

    practice

--- CodeSignals Practice/practice.py
+++ CodeSignals Practice/practice.py
@@ -5,3 +5,10 @@ def checkPalindrome(inputString):
         if inputString[i] != inputString[len(inputString)-i-1]:
             return False
     return True
+
+def largestProduct(inputArray):
+    large = -100000
+    for i in range(len(inputArray)-1):
+        if (large <= (inputArray[i]*inputArray[i+1])):
+            large = inputArray[i]*inputArray[i+1]
+    return large
