6a212460608ee8629c3d8951ef193289af3cf754
Jose Arnel <jonel.manipon@gmail.com>
2023-10-12 13:30:30 -0400

    CODEgit add .

--- CodeSignals Practice/practice.py
+++ CodeSignals Practice/practice.py
@@ -12,3 +12,15 @@ def largestProduct(inputArray):
         if (large <= (inputArray[i]*inputArray[i+1])):
             large = inputArray[i]*inputArray[i+1]
     return large
+
+def stringPairs(a, b, k):
+    count = 0
+    for i in range(0,len(a)):
+        x = str(a[i]) + str(b[len(a)-i-1])
+        if int(x) < k:
+            count += 1
+    return count
+
+##a: [1, 2, 3]
+##b: [1, 2, 3]
+##k: 31
