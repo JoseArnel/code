1f7971fd6d4323136b1c84bb1643711970746b8f
Jose Arnel <jonel.manipon@gmail.com>
2024-03-21 19:56:09 -0400

    commit3

--- LeetCode /SQL_leetcode.sql
+++ LeetCode /SQL_leetcode.sql
@@ -130,27 +130,14 @@ FROM Transactions
 GROUP BY month, country
 
 /* 1174. Immediate Food Delivery II */
-SELECT ROUND(AVG(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage
+SELECT ROUND(SUM(IF(order_date = customer_pref_delivery_date, 1, 0))*100 / COUNT(DISTINCT customer_id), 2) AS immediate_percentage
+-- SELECT AVG(SUM(IF(order_date = customer_pref_delivery_date, 1, 0))*100 / COUNT(DISTINCT customer_id), 2) AS immediate_percentage
 FROM Delivery 
-# sort earliest date
-WHERE (customer_id, order_date) in (
-    SELECT customer_id, min(order_date)
+WHERE (customer_id, order_date) IN (
+    SELECT customer_id, MIN(order_date)
     FROM Delivery
     GROUP BY customer_id
 )
-
-SELECT 
-    ROUND(SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END) * 100.0 / COUNT(DISTINCT customer_id), 2) AS immediate_percentage
-FROM Delivery
-WHERE (customer_id, order_date) IN (
-    SELECT customer_id, MIN(order_date) AS first_order_date
-    FROM Delivery
-    GROUP BY customer_id
-);
-
-
-
-
 
 
 595. Big Countries
