1fc4f6f6b41c4f0a4506d9dc46648806f72312a0
Jose Arnel <jonel.manipon@gmail.com>
2024-03-25 18:19:06 -0400

    commit2

--- LeetCode /SQL_leetcode.sql
+++ LeetCode /SQL_leetcode.sql
@@ -152,6 +152,11 @@ FROM Courses
 GROUP BY class
 HAVING COUNT(student) >= 5
 
+/* 1729. Find Followers Count  */
+SELECT user_id, COUNT(follower_id) as followers_count
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id ASC
 
 
 
