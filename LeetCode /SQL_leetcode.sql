3e0f60ef007e8e9527eff57bb47dee96fb447dbb
Jose Arnel <jonel.manipon@gmail.com>
2024-04-05 19:23:25 -0400

    april5

--- LeetCode /SQL_leetcode.sql
+++ LeetCode /SQL_leetcode.sql
@@ -169,11 +169,10 @@ FROM (
 as unique_max
 
 /* 1045. Customers Who Bought All Products */
-SELECT c.customer_id
-FROM Customer c
-INNER JOIN Product p ON c.product_key = p.product_key
-GROUP BY c.customer_id
-HAVING count(*) != (SELECT COUNT(*) p) 
+SELECT customer_id
+FROM Customer 
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (SELECT COUNT(product_key) FROM Product)
 /* review solutionn */
 
 
