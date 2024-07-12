e413e5bafa146f066118362bb6df620f81e85ae5
Jose Arnel <jonel.manipon@gmail.com>
2024-07-11 21:58:07 -0400

    july11

--- LeetCode /SQL_leetcode.sql
+++ LeetCode /SQL_leetcode.sql
@@ -344,6 +344,23 @@ ORDER BY id
 # Joins and Order bys, Somehow get Danials name andthe movie
 # Find name
 
+# Write your MySQL query statement below
+# 3 tables, User with greatest number of movies, in Tie return smallest name
+#find highest average rating in Feb 2020, in tie lexicographcailly
+# Joins and Order bys, Somehow get Danials name andthe movie
+# Solve seperately use UNION
+# Find name
+
+SELECT name
+FROM Users u 
+INNER JOIN MovieRating r ON u.user_id = r.user_id 
+WHERE name (SELECT name
+            FROM )
+GROUP BY name
+ORDER BY name
+
+# order by count(*) desc
+
 SELECT name
 FROM Users u
 INNER JOIN MovieRating m on m.user_id = u.user_id
