a53a2792c5062dfb1bba0b5758e11fb207fbb460
Jose Arnel <jonel.manipon@gmail.com>
2024-07-28 21:22:51 -0400

    july28

--- LeetCode /SQL_leetcode.sql
+++ LeetCode /SQL_leetcode.sql
@@ -416,8 +416,21 @@ GROUP BY 1
 ORDER BY 2
 DESC LIMIT 1
 
-
-raa
+/* 585. Investments in 2016 */
+SELECT ROUND(SUM(tiv_2016), 2) as tiv_2016
+FROM Insurance
+WHERE tiv_2015 IN (
+    SELECT tiv_2015
+    FROM Insurance 
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
+)
+AND (lat, lon) IN (
+    SELECT lat, lon
+    FROM Insurance 
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+)
 
 # order by count(*) desc
 #  order by average_rating desc, results asc limit 1
